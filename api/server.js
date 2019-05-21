acaebe2f7258b5984277e46c555a5715c0e396c1
keef-ui <keef-v@hotmail.co.uk>
2019-05-21 19:06:14 +0100

    setup express server with with basic '/' root

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { ApolloServer, gql } = require("apollo-server-express");
 const Cors= require('cors')
 const bodyParser = require('body-parser')
+
 const db = require('./sequelize')
 
 
@@ -9,6 +10,9 @@ const db = require('./sequelize')
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.status(200).send('Api server setup')
+})
 
 app.use(Cors());
 
