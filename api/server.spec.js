41c296c541e4d9d60a67b338a683c0be6c193d98
keef-ui <keef-v@hotmail.co.uk>
2019-05-21 19:09:54 +0100

    test 2 : Test graphQL paths - It should response the query {status}

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,13 +1,19 @@
 const request = require('supertest');
 const app = require('./server')
 const port = 3001
-app.listen(port, () => {
-    console.log(`Running on http://localhost:${port}/graphql`)
-})
-describe('Test the root path of express server', () => {
-    test('It should response the GET method', async () => {
+
+    describe('Test the root path of express server2', () => {
+    test('It should response the GET method', async (done) => {
         const response = await request(app).get('/');
         expect(response.statusCode).toBe(200);
-        console.log(response);
-    });
-})
+        expect(response.text).toBe('Api server setup');
+        done();
+        });
+    })
+    describe('Test graphQL paths', () => {
+        test('It should response the query {status}', async (done) => {
+            const response = await request(app).post('/graphql').send({query:'query {status}'});
+            expect(response.statusCode).toBe(200);
+            done();
+        });
+    })
