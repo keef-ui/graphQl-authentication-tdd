1141082b84721c0ca76989649f09129bef821d46
keef-ui <keef-v@hotmail.co.uk>
2019-06-04 20:05:58 +0100

    test 5,6,7 : Should throw error when email,name or password are not present '

--- api/server.spec.js
+++ api/server.spec.js
@@ -22,14 +22,41 @@ const port = 3001
     })
      describe('Register user testing', () => {
         test('Should return status code 200 and confirmation for valid input when user details are provided', async (done) => {
-           let payLoad= `mutation {createUser(name:"John",email:"j.ohn@hotmail.com",password:"letmeib"){name}
-}`;
+           let payLoad= `mutation {createUser(name:"John",email:"j.ohn@hotmail.com",password:"letmeib"){name}}`;
 
             const response = await request(app).post('/graphql').send({query:payLoad});
             expect(response.statusCode).toBe(200);
             // expect(response.text).toBe( "{\"data\":{\"status\":\"OK\"}}");
             let body=JSON.parse(response.text);
             expect(body.data.createUser.name).toBe('John');
+            done();
+        });
+
+        test('Should throw error when name is not present', async (done) => {
+           let payLoad= `mutation {createUser(email:"j.ohn@hotmail.com",password:"letmeib"){name}}`;
+
+            const response = await request(app).post('/graphql').send({query:payLoad});
+            expect(response.statusCode).toBe(200);
+            let body=JSON.parse(response.text);
+            expect(body.errors[0].message).toBe('Name not recieved');
+            done();
+        });
+        test('Should throw error when email is not present', async (done) => {
+           let payLoad= `mutation {createUser(name:"John",password:"letmeib"){name}}`;
+
+            const response = await request(app).post('/graphql').send({query:payLoad});
+            expect(response.statusCode).toBe(200);
+            let body=JSON.parse(response.text);
+            expect(body.errors[0].message).toBe('Email not recieved');
+            done();
+        });
+        test('Should throw error when password is not present', async (done) => {
+           let payLoad= `mutation {createUser(name:"John",email:"j.ohn@hotmail.com"){name}}`;
+
+            const response = await request(app).post('/graphql').send({query:payLoad});
+            expect(response.statusCode).toBe(200);
+            let body=JSON.parse(response.text);
+            expect(body.errors[0].message).toBe('Password not recieved');
             done();
         });
     })
